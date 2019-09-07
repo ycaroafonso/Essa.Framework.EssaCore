3e4b6f07a17ab61371790d8a9392dca47be73c69
Ycaro Afonso <ycaroafonso@outlook.com>
2019-09-07 10:06:19 -0400

    .

---
+++ Essa.Framework.MensageriaCore/CadastrarMensageria.cs
@@ -0,0 +1,87 @@
+﻿namespace Essa.Framework.MensageriaCore
+{
+    using RabbitMQ.Client;
+    using RabbitMQ.Client.Events;
+    using System;
+
+    public class CadastrarMensageria : IDisposable
+    {
+        private ConnectionFactory _factory;
+        private IConnection _connection;
+        private IModel _channel;
+        string _queue;
+
+        public CadastrarMensageria(string queue, bool autoDelete = false)
+        {
+            _factory = new ConnectionFactory() { HostName = "localhost" };
+            _connection = _factory.CreateConnection();
+            _channel = _connection.CreateModel();
+
+            _queue = queue;
+
+            _channel.QueueDeclare(queue: queue,
+                     durable: true,
+                     exclusive: false,
+                     autoDelete: autoDelete,
+                     arguments: null);
+        }
+
+
+        private ulong CodigoRecebimento;
+
+        public void Receber(Action<byte[]> received)
+        {
+            var consumer = new EventingBasicConsumer(_channel);
+            consumer.Received += (model, ea) =>
+            {
+                CodigoRecebimento = ea.DeliveryTag;
+
+                received(ea.Body);
+            };
+
+            _channel.BasicConsume(queue: _queue,
+                                 autoAck: false,
+                                 consumer: consumer);
+        }
+
+        public void ConfirmarRecebimento()
+        {
+            _channel.BasicAck(CodigoRecebimento, false);
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        public void Publicar(byte[] body)
+        {
+            _channel.BasicPublish(exchange: "",
+                            routingKey: _queue,
+                            basicProperties: null,
+                            body: body);
+        }
+
+
+
+
+
+
+
+
+        public void Dispose()
+        {
+            _connection.Dispose();
+            _channel.Dispose();
+        }
+    }
+}
