c07b2e6a85de1f020314d747476a23bf22128292
Ycaro Afonso <ycaroafonso@outlook.com>
2020-04-10 18:37:19 -0400

    Essa.Framework.Logger

---
+++ Essa.Framework.Logger/Geral.cs
@@ -0,0 +1,26 @@
+﻿namespace Essa.Framework.Logger
+{
+    using Serilog;
+    using Serilog.Events;
+
+
+    public static class Geral
+    {
+        public static Serilog.Core.Logger Log(string nomePrograma = "", string diretoriolog = "")
+        {
+            string arquivolog = diretoriolog;
+#if DEBUG
+            arquivolog += @"DEBUG\";
+#endif
+
+            return new LoggerConfiguration()
+.MinimumLevel.Debug()
+.MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+.Enrich.FromLogContext()
+.Enrich.WithProperty("Sistema", nomePrograma)
+.WriteTo.Console()
+.WriteTo.File($"{arquivolog}log_{nomePrograma}_.txt", rollingInterval: RollingInterval.Day)
+.CreateLogger();
+        }
+    }
+}
