5542e1f4a547bcbfd6d00c343be76d3abbb8fe7e
Ycaro Afonso <ycaroafonso@outlook.com>
2021-03-24 08:59:21 -0400

    .

---
+++ Essa.Framework.UtilCore/Models/Interface/IOptionsAutenticacao.cs
@@ -0,0 +1,9 @@
+﻿namespace Essa.Framework.Util.Models.Interface
+{
+    public interface IOptionsAutenticacao
+    {
+        string Usuario { get; set; }
+
+        string Senha { get; set; }
+    }
+}
