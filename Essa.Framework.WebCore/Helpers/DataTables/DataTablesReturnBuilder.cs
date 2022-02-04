eeacbbd3c1c6d1e9bebb031b6d4a626340b365c7
Ycaro <ycaroafonso@outlook.com>
2022-02-04 17:21:24 -0400

    DataTables

--- Essa.Framework.WebCore/Helpers/DataTables/DataTablesReturnBuilder.cs
+++ Essa.Framework.WebCore/Helpers/DataTables/DataTablesReturnBuilder.cs
@@ -1,108 +1,123 @@
-﻿//namespace SENAR.Framework.Web.Helpers.JqGrid
-//{
-//    using SENAR.Framework.Util;
-//    using System;
-//    using System.Collections.Generic;
-//    using System.Linq;
+﻿using DataTables.AspNet.AspNetCore;
+using DataTables.AspNet.Core;
+using Essa.Framework.Web.Helpers.DataTables;
+using SENAR.Framework.Util;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using static SENAR.Framework.Web.Helpers.DataTables.DataTablesUtil;
+
+namespace SENAR.Framework.Web.Helpers.DataTables
+{
+    public static class DataTablesUtil
+    {
+        public enum TipoFiltro
+        {
+            Contem,
+            Igual,
+            Customizado,
+
+            MaiorOuIgual,
+            MenorOuIgual
+        }
+    }
+
+    public class DataTablesReturnBuilder<T>
+    {
+        private int _qtdeTotal;
+        private IQueryable<T> _listaFiltrada;
+        private readonly IDataTablesRequest _request;
 
 
-//    public class DataTablesReturnBuilder<T>
-//    {
-//        private int _qtdeRegistro { get; set; }
-//        public JqGridReturn Retorna { get; set; }
-//        private GridSettings _GridSettings;
-//        private IQueryable<T> _lista;
+        public DataTablesReturnBuilder(IQueryable<T> lista, IDataTablesRequest request)
+        {
+
+            _qtdeTotal = lista.Count();
+
+            _listaFiltrada = lista;
+
+            _request = request;
+        }
+
+        public DataTablesReturnBuilder<T> AddFiltro(Expression<Func<T, bool>> filtro)
+        {
+            _listaFiltrada = _listaFiltrada.Where(filtro);
+            return this;
+        }
+
+        public IQueryable<T> RetornaConsulta()
+        {
+            return _listaFiltrada;
+        }
 
 
-//        public DataTablesReturnBuilder(IQueryable<T> lista, GridSettings gridSettings)
-//        {
-//            _GridSettings = gridSettings;
-//            _lista = lista;
 
 
-//            if (gridSettings.Where != null)
-//                foreach (var item in gridSettings.Where.rules.Where(c => c.data != ""))
-//                    switch (item.op)
-//                    {
-//                        case "eq":
-//                            _lista = _lista.Where(string.Concat(item.field, " = " + ProcessaValor(item.data)));
-//                            break;
-//                        case "cn":
-//                            _lista = _lista.Where(string.Concat(item.field, ".contains(\"", item.data, "\")"));
-//                            break;
-//                        default:
-//                            _lista = _lista.Where(string.Concat(item.field, " ", item.op, " ", ProcessaValor(item.data)));
-//                            break;
-//                    }
 
-//            _qtdeRegistro = _lista.Count();
-//            Retorna = new JqGridReturn
-//            {
-//                records = _qtdeRegistro,
-//                page = gridSettings.PageIndex,
-//                total = (int)Math.Ceiling(_qtdeRegistro / (decimal)gridSettings.PageSize),
-//            };
-//        }
+        public DataTablesReturnBuilder<T> AddFiltro<TParam>(string nomeFiltro, Func<TParam, Expression<Func<T, bool>>> filtro)
+        {
+            if (_request.AdditionalParameters.ContainsKey(nomeFiltro))
+            {
+                var d = _request.Get<TParam>(nomeFiltro);
+                var x = filtro(d);
+                _listaFiltrada = _listaFiltrada.Where(x);
+            }
 
-//        public JqGridReturnBuilder(List<T> lista, GridSettings gridSettings)
-//        {
-//            _qtdeRegistro = lista.Count();
-
-//            Retorna = new JqGridReturn
-//            {
-//                records = _qtdeRegistro,
-//                page = gridSettings.PageIndex,
-//                total = (int)Math.Ceiling(_qtdeRegistro / (decimal)gridSettings.PageSize),
-//                rows = lista
-//            };
-//        }
-
-//        public JqGridReturn ToReturn()
-//        {
-//            if (!string.IsNullOrEmpty(_GridSettings.SortColumn))
-//            {
-//                Retorna.rows = _lista.OrderBy(_GridSettings.SortColumn + " " + _GridSettings.SortOrder)
-//                    .Skip((_GridSettings.PageIndex - 1)*_GridSettings.PageSize)
-//                    .Take(_GridSettings.PageSize)
-//                    .ToList();
-//            }
-//            else
-//            {
-//                Retorna.rows = _lista.Skip((_GridSettings.PageIndex - 1) * _GridSettings.PageSize)
-//                    .Take(_GridSettings.PageSize)
-//                    .ToList();
-//            }
-
-//            return Retorna;
-//        }
-
-//        public JqGridReturn ToReturn<TResult>(Func<T, TResult> select)
-//        {
-//            if (!string.IsNullOrEmpty(_GridSettings.SortColumn))
-//            {
-//                Retorna.rows = _lista.OrderBy(_GridSettings.SortColumn + " " + _GridSettings.SortOrder)
-//                    .Skip((_GridSettings.PageIndex - 1) * _GridSettings.PageSize)
-//                    .Take(_GridSettings.PageSize)
-//                    .ToList().Select(select);
-//            }
-//            else
-//            {
-//                Retorna.rows = _lista.Skip((_GridSettings.PageIndex - 1) * _GridSettings.PageSize)
-//                    .Take(_GridSettings.PageSize)
-//                    .ToList().Select(select);
-//            }
-            
-//            return Retorna;
-//        }
+            return this;
+        }
 
 
-//        private string ProcessaValor(string valor)
-//        {
-//            int n;
-//            if (valor.Contains("DateTime") || int.TryParse(valor, out n))
-//                return valor;
-//            else
-//                return "\"" + valor + "\"";
-//        }
-//    }
-//}
+
+
+        string Operador(TipoFiltro tipoFiltro)
+        {
+            switch (tipoFiltro)
+            {
+                case TipoFiltro.Igual:
+                    return "=";
+                case TipoFiltro.MaiorOuIgual:
+                    return ">=";
+                case TipoFiltro.MenorOuIgual:
+                    return "<= ";
+                default:
+                    throw new Exception();
+            }
+        }
+
+
+        public DataTablesJsonResult ToReturn<TResult>(Func<T, TResult> select, IDictionary<string, object> additionalParameters = null)
+        {
+            var qtdeFiltrado = _listaFiltrada.Count();
+
+            var sortColumm = _request.Columns.FirstOrDefault(x => x.Sort != null);
+            if (sortColumm != null)
+            {
+                if (sortColumm.Sort.Direction == SortDirection.Descending)
+                    _listaFiltrada = _listaFiltrada.OrderBy(sortColumm.Name + " desc");
+                else
+                    _listaFiltrada = _listaFiltrada.OrderBy(sortColumm.Name);
+
+                _listaFiltrada = _listaFiltrada.Skip(_request.Start);
+
+                if (_request.Length > 0)
+                    _listaFiltrada = _listaFiltrada.Take(_request.Length);
+            }
+
+            var listaFinal = _listaFiltrada.ToList().Select(select);
+
+            var response = DataTablesResponse.Create(_request, _qtdeTotal, qtdeFiltrado, listaFinal, additionalParameters);
+            return new DataTablesJsonResult(response);
+        }
+
+
+        private string ProcessaValor(string valor)
+        {
+            int n;
+            if (valor.Contains("DateTime") || int.TryParse(valor, out n))
+                return valor;
+            else
+                return "\"" + valor + "\"";
+        }
+    }
+}
