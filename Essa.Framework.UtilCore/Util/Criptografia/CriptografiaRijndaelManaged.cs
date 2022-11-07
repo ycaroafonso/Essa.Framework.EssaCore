a43ffe62bba6d1ceece99c1f84c5b4f9ccef9db6
Ycaro Afonso <ycaroafonso@gmail.com>
2022-11-07 08:37:51 -0400

    .

--- Essa.Framework.UtilCore/Util/Criptografia/CriptografiaRijndaelManaged.cs
+++ Essa.Framework.UtilCore/Util/Criptografia/CriptografiaRijndaelManaged.cs
@@ -2,13 +2,14 @@
 {
     using System;
     using System.IO;
+    using System.Linq;
     using System.Security.Cryptography;
     using System.Text;
-
+    using System.Web;
 
     public class CriptografiaRijndaelManaged : IDisposable
     {
-        private RijndaelManaged _AES;
+        private Aes _AES;
 
         /// <summary>
         /// 
@@ -17,7 +18,7 @@
         /// <param name="IV">Chave interno: 16 bits</param>
         public CriptografiaRijndaelManaged(byte[] key, byte[] IV)
         {
-            _AES = new RijndaelManaged();
+            _AES = Aes.Create("AesManaged");
             _AES.Key = key;
             _AES.IV = IV;
         }
@@ -33,10 +34,36 @@
                 return cryptor.TransformFinalBlock(bytesToBeEncrypted, 0, bytesToBeEncrypted.Length);
         }
 
+
+        public string EncryptStr(byte[] bytesToBeEncrypted)
+        {
+            using (var cryptor = _AES.CreateEncryptor())
+            {
+                var x = cryptor.TransformFinalBlock(bytesToBeEncrypted, 0, bytesToBeEncrypted.Length);
+
+                string base64 = Convert.ToBase64String(x.ToArray());
+
+                // Generate a string that won't get screwed up when passed as a query string.
+                string urlEncoded = HttpUtility.UrlEncode(base64);
+                return urlEncoded;
+            }
+        }
+
         public byte[] DecryptFile(byte[] bytesToBeDecrypted)
         {
             using (var cryptor = _AES.CreateDecryptor())
                 return cryptor.TransformFinalBlock(bytesToBeDecrypted, 0, bytesToBeDecrypted.Length);
+        }
+
+
+        public string DecryptStr(byte[] bytesToBeDecrypted)
+        {
+            using (var cryptor = _AES.CreateDecryptor())
+            {
+                var x = cryptor.TransformFinalBlock(bytesToBeDecrypted, 0, bytesToBeDecrypted.Length);
+                string utf8 = Encoding.UTF8.GetString(x);
+                return utf8;
+            }
         }
 
         public void Dispose()
