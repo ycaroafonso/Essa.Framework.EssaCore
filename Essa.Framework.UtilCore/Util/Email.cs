ac44b51564d81547269bb054e02963e292985029
Ycaro Afonso <ycaroafonso@gmail.com>
2023-02-25 08:07:47 -0400

    Envio de email

--- Essa.Framework.UtilCore/Util/Email.cs
+++ Essa.Framework.UtilCore/Util/Email.cs
@@ -1,93 +1,95 @@
-﻿namespace Essa.Framework.Util
-{
-    using System.IO;
-    using System.Net.Mail;
-    using System.Net.Mime;
-    using System.Threading.Tasks;
+﻿//using System.IO;
+//using System.Net.Mail;
+//using System.Net.Mime;
+//using System.Security;
+//using System.Threading.Tasks;
 
-    public class Email
-    {
-        private readonly System.Net.NetworkCredential _credencial;
-        private SmtpClient _smtp;
-
-        MailMessage _mailMessage;
-
-        public bool EnableSsl { get; set; }
-
-        public Email Titulo(string titulo)
-        {
-            _mailMessage.Subject = titulo;
-
-            return this;
-        }
+//namespace Essa.Framework.Util
+//{
+//    public class Email
+//    {
+//        private SmtpClient _smtp;
+//        MailMessage _mailMessage;
 
 
-        public Email(string userName, string password, string servidor, int porta, string emailDe = null)
-        {
-            _credencial = new System.Net.NetworkCredential(userName, password);
-            _smtp = new SmtpClient(servidor, porta);
-            _smtp.EnableSsl = true;
-            _smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
-            _smtp.UseDefaultCredentials = true;
-            _smtp.Credentials = _credencial;
+//        public Email Titulo(string titulo)
+//        {
+//            _mailMessage.Subject = titulo;
 
-            _mailMessage = new MailMessage();
-
-            _mailMessage.From = new MailAddress(emailDe ?? userName);
-        }
+//            return this;
+//        }
 
 
-        public Email Conteudo(string cont, bool IsBodyHtml)
-        {
-            _mailMessage.Body = cont;
-            _mailMessage.IsBodyHtml = IsBodyHtml;
+//        public Email(string userName, string password, string servidor, int porta, string emailDe = null, string domain = null, bool enableSsl = true)
+//        {
+//            _smtp = new SmtpClient(servidor)
+//            {
+                
+//                EnableSsl = enableSsl,
+//                //_smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+//                //_smtp.UseDefaultCredentials = false;
+                
+//                Credentials = new System.Net.NetworkCredential(userName, password, domain)
+//            };
 
-            return this;
-        }
+//            _mailMessage = new MailMessage();
 
-        public Email AddDestinatario(string email)
-        {
-            _mailMessage.To.Add(email);
-
-            return this;
-        }
-        public Email AddDestinatarioCc(string email)
-        {
-            _mailMessage.CC.Add(email);
-
-            return this;
-        }
-
-        public Email AddDestinatarioCco(string email)
-        {
-            _mailMessage.Bcc.Add(email);
-
-            return this;
-        }
+//            _mailMessage.From = new MailAddress(emailDe ?? userName);
+//        }
 
 
-        public Email AddArquivoIncorporado(string conteudo, string cid, byte[] imagem, string contentType)
-        {
-            MemoryStream image1 = new MemoryStream(imagem);
-            AlternateView av = AlternateView.CreateAlternateViewFromString(conteudo, null, MediaTypeNames.Text.Html);
+//        public Email Conteudo(string cont, bool IsBodyHtml)
+//        {
+//            _mailMessage.Body = cont;
+//            _mailMessage.IsBodyHtml = IsBodyHtml;
 
-            LinkedResource headerImage = new LinkedResource(image1);
-            headerImage.ContentId = cid;
-            headerImage.ContentType = new ContentType(contentType);
-            av.LinkedResources.Add(headerImage);
-            _mailMessage.AlternateViews.Add(av);
+//            return this;
+//        }
 
-            return this;
-        }
+//        public Email AddDestinatario(string email)
+//        {
+//            _mailMessage.To.Add(email);
+
+//            return this;
+//        }
+//        public Email AddDestinatarioCc(string email)
+//        {
+//            _mailMessage.CC.Add(email);
+
+//            return this;
+//        }
+
+//        public Email AddDestinatarioCco(string email)
+//        {
+//            _mailMessage.Bcc.Add(email);
+
+//            return this;
+//        }
 
 
-        public void Enviar()
-        {
-            _smtp.Send(_mailMessage);
-        }
-        public async Task EnviarAsync()
-        {
-            await _smtp.SendMailAsync(_mailMessage);
-        }
-    }
-}
+//        public Email AddArquivoIncorporado(string conteudo, string cid, byte[] imagem, string contentType)
+//        {
+//            MemoryStream image1 = new MemoryStream(imagem);
+//            AlternateView av = AlternateView.CreateAlternateViewFromString(conteudo, null, MediaTypeNames.Text.Html);
+
+//            LinkedResource headerImage = new LinkedResource(image1);
+//            headerImage.ContentId = cid;
+//            headerImage.ContentType = new ContentType(contentType);
+//            av.LinkedResources.Add(headerImage);
+//            _mailMessage.AlternateViews.Add(av);
+
+//            return this;
+//        }
+
+
+//        public void Enviar()
+//        {
+//            _smtp.Send(_mailMessage);
+//        }
+
+//        public async Task EnviarAsync()
+//        {
+//            await _smtp.SendMailAsync(_mailMessage);
+//        }
+//    }
+//}
