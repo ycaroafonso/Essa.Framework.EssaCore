c5d23ee7883f87f3b2ef990ad359f90ab6a58bb4
Ycaro Afonso <ycaroafonso@gmail.com>
2023-05-15 03:45:32 +0000

    Validar email

---
+++ Essa.Framework.UtilCore/Util/EmailUtil.cs
@@ -0,0 +1,23 @@
+﻿using System.Net.Mail;
+
+namespace Essa.Framework.Util.Util
+{
+    public static class EmailUtil
+    {
+        public static bool IsValid(string email)
+        {
+            var valid = true;
+
+            try
+            {
+                var emailAddress = new MailAddress(email);
+            }
+            catch
+            {
+                valid = false;
+            }
+
+            return valid;
+        }
+    }
+}
