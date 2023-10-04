7764dcf62814571add50448b46f2cc76e5c1fd03
Ycaro Afonso <ycaroafonso@gmail.com>
2023-10-03 23:17:41 -0400

    IGenericTransactionRepository

--- Essa.Framework.UtilSqlServer/Repository/IGenericTransactionRepository.cs
+++ Essa.Framework.UtilSqlServer/Repository/IGenericTransactionRepository.cs
@@ -6,5 +6,6 @@
     public interface IGenericTransactionRepository : IDisposable
     {
         IDbContextTransaction BeginTransaction();
+        Task<IDbContextTransaction> BeginTransactionAsync();
     }
 }
