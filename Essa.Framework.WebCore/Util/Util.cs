6e94ef6ed9e33e0b574e6855d7d6cd23b2f65d27
Ycaro Afonso <ycaroafonso@gmail.com>
2024-01-02 01:40:51 -0400

    Helpers para atender a migração do projeto em .NET Framework

--- Essa.Framework.WebCore/Util/Util.cs
+++ Essa.Framework.WebCore/Util/Util.cs
@@ -1,18 +1,35 @@
 ﻿namespace Essa.Framework.Web.Util
 {
+    using Essa.Framework.Util.Models.Helpers.Select2;
     using Extensions;
     using Microsoft.AspNetCore.Mvc.Rendering;
+    using System;
+    using System.Collections.Generic;
 
     public static class Util
     {
 
-        //public static SelectList SimNao()
-        //{
-        //    return Framework.Util.Geral.SimNao().ToSelectList(x => x.Key, y => y.Value);
-        //}
-        //public static SelectList SimNao(string valorSelecionado)
-        //{
-        //    return Framework.Util.Geral.SimNao().ToSelectList(x => x.Key, y => y.Value, null, null, valorSelecionado);
-        //}
+        public static List<SelectListItem> SimNao()
+        {
+            return Framework.Util.Geral.SimNao().ToSelectList(x => x.Key, y => y.Value);
+        }
+        public static List<SelectListItem> SimNao(string valorSelecionado)
+        {
+            return Framework.Util.Geral.SimNao().ToSelectList(x => x.Key, y => y.Value, null, null, valorSelecionado);
+        }
+        public static List<SelectListItem> SimNaoBooleano()
+        {
+            return Framework.Util.Geral.SimNaoBooleano().ToSelectList(x => x.Key, y => y.Value);
+        }
+        public static List<SelectListItem> SimNaoBooleano(string valorSelecionado)
+        {
+            return Framework.Util.Geral.SimNaoBooleano().ToSelectList(x => x.Key, y => y.Value, null, null, valorSelecionado);
+        }
+
+        [Obsolete("Retirar este método e utilizar javascript.")]
+        public static List<SelectListItem> SimNaoBooleanoWithLowerCaseInValue()
+        {
+            return Framework.Util.Geral.SimNaoBooleano().ToSelectList(x => x.Key.ToString().ToLower(), y => y.Value);
+        }
     }
 }
