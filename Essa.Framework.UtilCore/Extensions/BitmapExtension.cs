d20deb2ca85656b36ab04881f7c1479dc8c510bf
Ycaro Afonso <ycaroafonso@gmail.com>
2024-09-01 23:43:27 -0400

    Reserva

---
+++ Essa.Framework.UtilCore/Extensions/BitmapExtension.cs
@@ -0,0 +1,17 @@
+﻿using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+
+namespace Essa.Framework.Util.Extensions;
+
+public static class BitmapExtension
+{
+    public static byte[] BitmapToByteArray(this Bitmap bitmap)
+    {
+        using (MemoryStream ms = new MemoryStream())
+        {
+            bitmap.Save(ms, ImageFormat.Png);
+            return ms.ToArray();
+        }
+    }
+}
