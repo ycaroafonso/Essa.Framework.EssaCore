1fa39329de50eae2ae63fdd0a343037ab6efe664
Ycaro Afonso <ycaroafonso@gmail.com>
2024-10-28 19:11:02 -0400

    Conciliação com filtro por centro de custo, conta gerencial e conta

--- Essa.Framework.UtilCore/Models/Interface/IPessoaSessaoViewModel.cs
+++ Essa.Framework.UtilCore/Models/Interface/IPessoaSessaoViewModel.cs
@@ -1,29 +1,29 @@
 ﻿using System.Collections.Generic;
 
-namespace Essa.Framework.Util.Models.Interface
+namespace Essa.Framework.Util.Models.Interface;
+
+public interface IPessoaSessaoViewModel
 {
-    public interface IPessoaSessaoViewModel
-    {
-        int EmpresaId { get; set; }
-        string NomeEmpresa { get; set; }
+    int EmpresaId { get; set; }
+    string NomeEmpresa { get; set; }
 
-        string NomePessoa { get; set; }
-        int PessoaId { get; set; }
+    string NomePessoa { get; set; }
+    int PessoaId { get; set; }
 
-        string Login { get; set; }
+    string Login { get; set; }
 
-        int UsuarioId { get; set; }
-        IPessoaPermissaoSessaoViewModel Permissao { get; set; }
-    }
+    int UsuarioId { get; set; }
+    IPessoaPermissaoSessaoViewModel Permissao { get; set; }
+}
 
-    public interface IPessoaPermissaoSessaoViewModel
-    {
-        IList<int> PessoaIdsLocal { get; set; }
-        IList<int> ContaIds { get; set; }
-
-    }
-    public interface IParametroPessoaViewModel
-    {
-        IPessoaSessaoViewModel PessoaSessaoViewModel { get; set; }
-    }
+public interface IPessoaPermissaoSessaoViewModel
+{
+    IList<int> PessoaIdsLocal { get; set; }
+    IList<int> ContaIds { get; set; }
+    IList<int> CentroCustoIds { get; set; }
+    IList<int> ContaGerencialds { get; set; }
+}
+public interface IParametroPessoaViewModel
+{
+    IPessoaSessaoViewModel PessoaSessaoViewModel { get; set; }
 }
