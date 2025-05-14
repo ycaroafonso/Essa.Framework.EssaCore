3b7d504127ca18103c6a3f4547a70f463d6edbd1
Ycaro Souza <ycaroafonso@gmail.com>
2025-05-14 11:25:43 -0400

    EmailDTO

---
+++ Essa.Framework.UtilCore/Models/Email/EmailArquivoIncorporadoDTO.cs
@@ -0,0 +1,16 @@
+﻿namespace Essa.Framework.Util.Models.Email
+{
+    public class EmailArquivoIncorporadoDTO
+    {
+        public EmailArquivoIncorporadoDTO(string cid, byte[] imagem, string contentType)
+        {
+            CID = cid;
+            Imagem = imagem;
+            ContentType = contentType;
+        }
+
+        public string CID { get; }
+        public byte[] Imagem { get; }
+        public string ContentType { get; }
+    }
+}
