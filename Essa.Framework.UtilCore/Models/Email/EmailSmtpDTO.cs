742ca7e2dc78ca35e86e99b81c80f655c0853e8c
Ycaro Souza <ycaroafonso@gmail.com>
2025-05-15 16:07:15 -0400

    EmailSmtpDTO

---
+++ Essa.Framework.UtilCore/Models/Email/EmailSmtpDTO.cs
@@ -0,0 +1,19 @@
+﻿namespace Essa.Framework.Util.Models.Email;
+
+/// <summary>
+/// Ycaro Afonso
+/// 16/02/2020
+/// </summary>
+public class EmailSmtpDTO
+{
+    public string UserName { get; set; }
+    public string Password { get; set; }
+    public string Servidor { get; set; }
+    public int Porta { get; set; }
+    public string EmailDe { get; set; }
+    public bool EnableSsl { get; set; } = true;
+    public string Dominio { get; set; }
+
+    public TipoSegurancaEnum? TipoSeguranca { get; set; } = TipoSegurancaEnum.SslOnConnect;
+}
+
