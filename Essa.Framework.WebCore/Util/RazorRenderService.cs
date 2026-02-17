fb4b8e413632453643409438caf763f49d8f7344
Ycaro Afonso <ycaroafonso@gmail.com>
2026-02-17 12:56:57 -0400

    RazorRender

---
+++ Essa.Framework.WebCore/Util/RazorRenderService.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc.Razor;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+
+namespace Essa.Framework.Web.Util;
+
+public interface IRazorRenderService
+{
+    Task<string> RenderToStringAsync(string viewName, object model);
+}
+public class RazorRenderService : IRazorRenderService
+{
+    private readonly IRazorViewEngine _viewEngine;
+    private readonly ITempDataProvider _tempDataProvider;
+    private readonly IServiceProvider _serviceProvider;
+
+    public RazorRenderService(IRazorViewEngine viewEngine, ITempDataProvider tempDataProvider, IServiceProvider serviceProvider)
+    {
+        _viewEngine = viewEngine;
+        _tempDataProvider = tempDataProvider;
+        _serviceProvider = serviceProvider;
+    }
+
+    public async Task<string> RenderToStringAsync(string viewName, object model)
+    {
+        var actionContext = new ActionContext(
+            new DefaultHttpContext { RequestServices = _serviceProvider },
+            new Microsoft.AspNetCore.Routing.RouteData(),
+            new Microsoft.AspNetCore.Mvc.Abstractions.ActionDescriptor()
+        );
+
+        var viewResult = _viewEngine.FindView(actionContext, viewName, false);
+
+        if (viewResult.View == null)
+            throw new ArgumentNullException($"{viewName} não encontrada.");
+
+        await using var sw = new StringWriter();
+
+        var viewDictionary = new ViewDataDictionary(new EmptyModelMetadataProvider(), new ModelStateDictionary())
+        {
+            Model = model
+        };
+
+        var viewContext = new ViewContext(
+            actionContext,
+            viewResult.View,
+            viewDictionary,
+            new TempDataDictionary(actionContext.HttpContext, _tempDataProvider),
+            sw,
+            new HtmlHelperOptions()
+        );
+
+        await viewResult.View.RenderAsync(viewContext);
+        return sw.ToString();
+    }
+}
